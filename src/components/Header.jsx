082d4184ad58cb97e144342a8b70870f91eb6b95
helladyo <heladiofog@gmail.com>
2021-08-25 13:21:52 -0500

    UseState example with thr header var for Dark mode.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import { useState, useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 const Header = () => {
   // state hooks
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);  // initial state is false
+  // Context vars for being used here
+  const color = useContext(ThemeContext);
 
   // handlers
   const handleClick = () => {
     setDarkMode(!darkMode);
   }
+
   return (
     <div className="Header">
-      <h1>React Hooks</h1>
-      <button type="button" onClick={handleClick}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
-      <button type="button" onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Light Mode2' : 'Dark Mode2'}</button>
-
+      <h1 style={{ color }}>React Hooks from Platzi</h1>
+      <button type="button" onClick={handleClick}>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+      {/* with anonymous function */}
+      {/* <button type="button" onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Light Mode2' : 'Dark Mode2'}</button> */}
     </div>
   );
 }
